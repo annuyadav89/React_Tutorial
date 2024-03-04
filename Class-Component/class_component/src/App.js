ab2802bdcf2d691f8d36239f634fec7b14ab0520
annuyadav89 <annuy337@gmail.com>
2024-03-04 10:43:43 +0530

    done

--- Class-Component/class_component/src/App.js
+++ Class-Component/class_component/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  // properties
+
+  // constructor
+
+//Declare state variable inside the constructor by using this keyword.
+  constructor(){
+    super()
+    this.state = {
+        count : 0
+    }
+  }
+
+  // method
+
+  render(){
+    return(<>
+       
+       
+      <h1>Count is :  {this.state.count}</h1>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
